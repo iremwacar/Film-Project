6393451a8204d3cc05e4e83c512558796c37d9b0
irem acar <iremmacar35@gmail.com>
2024-05-02 09:03:36 +0300

    finaly project

--- UI.js
+++ UI.js
@@ -18,3 +18,40 @@ UI.prototype.clearInputs = function (element1,element2,element3){
     element2.value = "";
     element3.value = "";
 }
+UI.prototype.displayMassages = function(message,type){
+    const cardBody = document.querySelector(".card-body");
+
+    //Alert div
+    const div = document.createElement("div");
+    div.className = `alert alert-${type}`;
+    div.textContent = message;
+
+    cardBody.appendChild(div);
+    setTimeout(function(){
+        div.remove();
+    },1000);
+}
+UI.prototype.loadAllFilms = function (films){
+    const filmList = document.getElementById("films");
+    films.forEach(function(film){
+        filmList.innerHTML += `
+        <tr>
+        <td><img src="${film.url}" class="img-fluid img-thumbnail"></td>
+        <td>${film.title}</td>
+        <td>${film.director}</td>
+        <td><a href="#" id = "delete-film" class = "btn btn-danger">Filmi Sil</a></td>
+        </tr>
+        `
+    });
+} 
+
+UI.prototype.deleteFilmFromUI = function(element){
+    element.parentElement.parentElement.remove();
+}
+
+UI.prototype.clearAllFilmsFromUI = function(){
+    const filmList = document.getElementById("films");
+    while(filmList.firstElementChild !== null){
+        filmList.firstElementChild.remove();
+    }
+}
--- project.js
+++ project.js
@@ -2,15 +2,25 @@ const form = document.getElementById("film-form");
 const titleElement = document.querySelector("#title");
 const directorElement = document.querySelector("#director");
 const urlElement = document.querySelector("#url");
-
+const cardBody = document.querySelectorAll(".card-body")[1];
+const clear = document.getElementById("clear-films");
 //Start UI Object
 const ui = new UI();
+
+//Start Storage object
+const storage = new Storage();
 
 //upload all events
 eventListeners();
 
 function eventListeners(){
     form.addEventListener("submit",addFilm);
+    document.addEventListener("DOMContentLoaded",function(){
+        let films = storage.getFilmsFromStorage();
+        ui.loadAllFilms(films);
+    });
+    cardBody.addEventListener("click",deleteFilm);
+    clear.addEventListener("click",clearAllFilms);
 }
 
 function addFilm(e){
@@ -20,6 +30,8 @@ function addFilm(e){
 
     if(title==="" || director==="" || url===""){
         //Eror
+        ui.displayMassages("Tüm alanları doldurunuz...","danger");
+
     }
     else{
         //new film
@@ -27,8 +39,31 @@ function addFilm(e){
 
         //adding film to the UI
         ui.addFilmToUI(newFilm);
+        //adding film to the Storage
+        storage.addFilmToStorage(newFilm);
+        ui.displayMassages("Film başarı ile eklendi...","success");
     }
 
     ui.clearInputs(titleElement,urlElement,directorElement);
     e.preventDefault();
+}
+
+function deleteFilm(e){
+    if(e.target.id ==="delete-film"){
+        ui.deleteFilmFromUI(e.target);
+        storage.deleteFilmFromStorage(e.target.parentElement.previousElementSibling.previousElementSibling.textContent);
+        ui.displayMassages("Silme işlemi başarılı...","success");
+    }
+}
+
+function clearAllFilms(){
+    if(confirm("Emin misiniz?")){
+        ui.clearAllFilmsFromUI();
+        storage.clearAllFilmsFromStorage();
+        ui.displayMassages("Tüm filmler başarı ile silindi...","success")
+    }
+    else{
+        ui.displayMassages("Işlem iptal edildi...","primary");
+    }
+    
 }
